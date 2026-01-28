9a6f809d8ac59873504c074d0f65a2ec59b93c24
ROYALKINGSJ <4667590+ROYALKINGSJ@users.noreply.github.com>
2026-01-29 00:28:22 +0530

    Added files 4,5,6 to week 2

---
+++ JAVA_CODES/WEEK_2/WEEK_2_4.java
@@ -0,0 +1,37 @@
+package OOPS_LAB;
+import java.util.Scanner;
+
+public class WEEK_2_4 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the Dimenation of Matrix : ");
+        int n=sc.nextInt();
+
+        int[][] arr=new int[n][n];
+
+        int sum=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Enter Element [" + (i+1) + "] [" + (j+1) + "] : ");
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("********************");
+
+        for (int i=0;i<n;i++) {
+            for (int j=n-1;j>=0;j--) {
+                if ((i+j)==(n-1)){
+                    System.out.print(arr[i][j] + " ");
+                    sum=sum+arr[i][j];
+                }
+            }
+        }
+        
+        System.out.println("SUM : "+sum);
+        sc.close();
+    }
+}
---
+++ JAVA_CODES/WEEK_2/WEEK_2_6.java
@@ -0,0 +1,25 @@
+package OOPS_LAB;
+import java.util.Scanner;
+
+public class WEEK_2_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Electricity Units Consumption[7 Days] : ");
+        float Electricity_Units_Consumption = sc.nextFloat();
+
+        sc.close();
+
+        switch ((int)((Electricity_Units_Consumption-1)/(100))) {
+            case 0:
+                System.out.println("Total Bill : "+Electricity_Units_Consumption*7);
+                break;
+            case 1:
+                System.out.println("Total Bill : "+Electricity_Units_Consumption*8);
+                break;
+            default:
+                System.out.println("Total Bill : "+Electricity_Units_Consumption*10);
+                break;
+        }
+    }    
+}
