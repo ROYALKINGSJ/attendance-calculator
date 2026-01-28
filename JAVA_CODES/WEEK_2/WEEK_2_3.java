2dda289fbd37febdb3f584d28b8d31623b3c348f
ROYALKINGSJ <4667590+ROYALKINGSJ@users.noreply.github.com>
2026-01-28 23:27:40 +0530

    added file text for week 2 file number 3

---
+++ JAVA_CODES/WEEK_2/WEEK_2_3.java
@@ -0,0 +1,19 @@
+package OOPS_LAB;
+import java.util.Scanner;
+
+public class WEEK_2_3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many Years have you already served : ");
+        int years_serviced=sc.nextInt();
+
+        System.out.println("Year of Birth : ");
+        int year_of_birth=sc.nextInt();
+
+        sc.close();
+
+        System.out.println("You have "+((year_of_birth+60)-(year_of_birth+18+years_serviced))+" years of service left before Retirement(if you joined at the age of 18)");
+    }
+}
